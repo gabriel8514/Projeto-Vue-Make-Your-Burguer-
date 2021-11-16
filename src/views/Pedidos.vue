7a86c66db68ce2a47cf13f4f93f3e48125bdd8f3
gabriel8514 <gabrielluizreis936@hotmail.com>
2021-11-15 23:41:44 -0300

    Projeto VueJS

---
+++ src/views/Pedidos.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="main-container">
+    <h1>Gerenciar pedidos:</h1>
+    <Deshboard/>
+  </div>
+</template>
+
+<script>
+import Deshboard from '../components/Deshboard.vue'
+
+export default {
+  name: "Pedidos",
+    components: { 
+      Deshboard 
+    }
+}
+</script>
